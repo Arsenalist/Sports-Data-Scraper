9dfec69964fd919cab63cbe95d0c51137037547c
Zarar Siddiqi <zarar@zarar-ubuntu64.(none)>
2014-05-11 00:05:42 -0400

    * pip freeze > requirements.txt * command-line text

--- cmd.py
+++ cmd.py
@@ -1,5 +1,5 @@
 import argparse
-import parser
+import sportsparser
 import os
 import shutil
 import time
@@ -9,18 +9,17 @@ import zipfile
 p = argparse.ArgumentParser()
 p.add_argument("date", help="Format is mmddyyyy")
 args = p.parse_args()
-c = parser.Controller()
+c = sportsparser.Controller()
 date_obj = time.strptime(args.date, "%m%d%Y")
 
 # convert to espn format
 date = time.strftime("%Y%m%d", date_obj)
 
 # generate files
-(dir, zip) = c.generate_playbyplay_csv(date)
+zip = c.generate_playbyplay_csv(date)
 
 #unzip the file and remove the directory, and then the zip file
 zipf = zipfile.ZipFile(zip)
-shutil.rmtree(dir)
 zipf.extractall()
 os.remove(zip)
 
