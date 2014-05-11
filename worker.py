1eccf7185e6036529c0eb0322cee2054ad86f1e6
Zarar Siddiqi <zarar@zarar-ubuntu64.(none)>
2014-05-11 09:49:42 -0400

    Debug.

--- worker.py
+++ worker.py
@@ -1,8 +1,9 @@
-import os
+	import os
 
 import redis
 from rq import Worker, Queue, Connection
-
+print "in worker"
+sys.stdout.flush()
 listen = ['high', 'default', 'low']
 
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
@@ -10,6 +11,8 @@ redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
+	print "in main"
+	sys.stdout.flush()
     with Connection(conn):
         worker = Worker(map(Queue, listen))
         worker.work()
